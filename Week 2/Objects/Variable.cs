fd4abcd647169f376d96270599817b161a44dc9c
Rogerium2102 <MatthewR2110@outlook.com>
2024-10-07 16:05:02 +0100

    1

--- Week 2/Objects/Variable.cs
+++ Week 2/Objects/Variable.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Week_2.Objects
 {
-    internal class Variable
+
+    public class Variable
     {
+        private int Value;
+        private string Identifier;
+
+        public Variable() { }   
+
+        public void SetValue(int value) {
+            Value = value; }
+
+        public int GetValue() { return Value; }
+
+        public void SetIdentifier(string identifier) { Identifier = identifier; }
+
+        public string GetIdentifier() { return Identifier; }
     }
 }
